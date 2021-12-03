9f239f129c7c663c9660de0d4e1a0fad3fd14d60
teejay80s <works.harbidoye@gmail.com>
2021-12-03 11:28:31 +0100

    Add comments to questionAction

--- src/redux/actions/questionAction.js
+++ src/redux/actions/questionAction.js
@@ -5,7 +5,9 @@ import {
     QUESTION_FAIL,
 } from  "../constants/questionConstant";
 
+// create variable
 export const getTestquestion = () => async (dispatch, getState) => {
+  // definining action i.e question request
     try {
       dispatch({ type: QUESTION_REQUEST });
       const {
@@ -13,6 +15,8 @@ export const getTestquestion = () => async (dispatch, getState) => {
         userLogin: { userInfo },
   
       } = getState();
+
+    // create a variable to fetch user token 
     const config = {
   
         headers: {
@@ -26,16 +30,20 @@ export const getTestquestion = () => async (dispatch, getState) => {
         },
   
       };
-      
-      const { data } = await axios.get("/api/v1/question/{id}", config);
+      // variable "data" is created for as the name of data coming from the back end
+      // = get those data from the api in the bracket
+      const { data } = await axios.get("/api/v1/question/id", config);
       dispatch({
+      // the line below shows if its successfull or not,if it is successful then load the data
         type: QUESTION_SUCCESS,
         payload: data,
       });
 
       console.log(data)
+      // if it isnt successful then catch type of error in other to know what type of error
     } catch (error) {
       dispatch({
+        // the line below shows failed and the payload will display the error
         type: QUESTION_FAIL,
         payload:
           error.response && error.response.data.error
