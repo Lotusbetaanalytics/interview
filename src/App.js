787a36aceb2f38d2b256c09e451cb4b6d111f77e
teejay80s <works.harbidoye@gmail.com>
2021-12-22 17:19:04 +0100

    merge app.js and index.css

--- src/App.js
+++ src/App.js
@@ -9,14 +9,23 @@ import SuccessScreen from "./screens/User/SuccessScreen";
 import Forget from "./screens/User/forget";
 import ChangePassword from "./screens/User/changePassword";
 import Sectionpage from "./screens/User/sectionpage";
+import Dashboard from "./screens/Dashboard";
+import AdminRegister from "./screens/AdminRegister";
+import ViewQuestion from "./screens/ViewQuestion";
+import Profile from "./screens/Profile";
+import QuestionBank from "./screens/QuestionBank";
+import AdminLogin from "./screens/AdminLogin";
+import Test from "./screens/Test";
+import Section from "./screens/Section";
+import ViewAdmin from "./screens/ViewAdmin";
+import PasswordForgot from "./screens/PasswordForgot";
+import ResetPassword from "./screens/ResetPassword";
 
 //import protectedRoute from "./components/protectedRoute";
-
 
 function App() {
   return (
     <Router>
-      
       <Switch>
         <Route path="/" exact component={WelcomeScreen} />
         <Route path="/login" exact component={LoginScreen} />
@@ -27,7 +36,19 @@ function App() {
         <Route path="/forget" exact component={Forget} />
         <Route path="/changePassword" exact component={ChangePassword} />
         <Route path="/sectionpage" exact component={Sectionpage} />
-        
+        <Route path="/adminlogin" exact component={AdminLogin} />
+        <Route path="/dashboard" exact component={Dashboard} />
+        <Route path="/adminregister" exact component={AdminRegister} />
+        <Route path="/profile" exact component={Profile} />
+        <Route path="/questionbank" exact component={QuestionBank} />
+        <Route path="/viewquestion" exact component={ViewQuestion} />
+        <Route path="/adminTest" exact component={Test} />
+        <Route path="/section" exact component={Section} />
+        <Route path="/viewadmin" exact component={ViewAdmin} />
+
+        <Route path="/passwordforgot" exact component={PasswordForgot} />
+
+        <Route path="/resetpassword" exact component={ResetPassword} />
       </Switch>
     </Router>
   );
